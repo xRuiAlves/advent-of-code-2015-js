232b5a73560935f0f878f72ec3c2b00c75af5cf9
xRuiAlves <up201606746@fe.up.pt>
2019-06-23 18:57:13 +0100

    Day 20 Part 2

--- Day 20/part2.js
+++ Day 20/part2.js
@@ -2,11 +2,12 @@ const reader = require("../utils/reader");
 
 const main = (data) => {
     const lower_bound = parseInt(data[0]);
-    const num_houses = lower_bound/10;
-    const houses = new Array(num_houses ).fill(0); 
+    const num_houses = Math.ceil(lower_bound/11);
+    const houses = new Array(num_houses).fill(0); 
     for (let elf_num = 1; elf_num <= num_houses; elf_num++) {
-        for (let house_num = elf_num; house_num <= num_houses; house_num += elf_num) {
-            houses[house_num-1] += elf_num*10;
+        let num_visited_houses = 0;
+        for (let house_num = elf_num; num_visited_houses < 50 && house_num <= num_houses; house_num += elf_num, num_visited_houses++) {
+            houses[house_num-1] += elf_num*11;
         }
     }
     for (let i in houses) {
